10dce73465b6b25cb6529ba226201ae40df9414b
Jason Zheng <zzheng3@stuy.edu>
2018-09-19 21:46:04 -0400

    realizes that there is no way to use the function

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,8 +1,10 @@
 public class CtoFTester{
-    public static double celsiusToFahrenheit (double c) {
-	return c * 1.8 + 32;
+    public static void main(String[] args){
+	public static double celsiusToFahrenheit (double c) {
+		return c * 1.8 + 32;
+		}
+    	public static double fahrenheitToCelsius (double f) {
+		return (f - 32) / 1.8;
+    		}
 	}
-    public static double fahrenheitToCelsius (double f) {
-	return (f - 32) / 1.8;
-    	}
 }

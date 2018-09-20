47a0f80f49e3afef6dfce8b606572be9c68b7ba6
Jason Zheng <zzheng3@stuy.edu>
2018-09-19 22:08:05 -0400

    wrote three separate functions

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,10 +1,17 @@
 public class CtoFTester{
     public static void main(String[] args){
-	public static double celsiusToFahrenheit (double c) {
-		return c * 1.8 + 32;
-		}
-    	public static double fahrenheitToCelsius (double f) {
-		return (f - 32) / 1.8;
-    		}
-	}
+	System.out.println("Use celsiusToFahrenheit or fahrenheitToCelsius");
+    }
 }
+
+public double celsiusToFahrenheit(double c){
+	return c * 1.8 + 32;
+}
+   
+public double fahrenheitToCelsius(double f){
+	return (f - 32) / 1.8;
+    }
+}
+
+	
+

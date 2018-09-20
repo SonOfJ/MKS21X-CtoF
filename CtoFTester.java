1391502c382ca3dbda4bf6cd8f9fd2cbedd7b2a2
Jason Zheng <zzheng3@stuy.edu>
2018-09-19 22:17:22 -0400

    added scanner for user input

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,17 +1,25 @@
+import java.util.Scanner;
+
 public class CtoFTester{
     public static void main(String[] args){
-	System.out.println("Use celsiusToFahrenheit or fahrenheitToCelsius");
-    }
-}
-
-public double celsiusToFahrenheit(double c){
-	return c * 1.8 + 32;
-}
-   
-public double fahrenheitToCelsius(double f){
-	return (f - 32) / 1.8;
-    }
-}
-
+	double temperature;
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Press 1 if the temperature is in Celsius.");
+	System.out.println("Press 2 if the temperature is in Fahrenheit.");
+	int choice = scan.nextInt();
 	
+	if (choice == 1) {
+		System.out.println("Enter temperature.");
+		temperature = scan.nextDouble();
+		System.out.println(temperature * 1.8 + 32);
+	}
 
+	if (choice == 2) {
+		System.out.println("Enter temperature.");
+		temperature = scan.nextDouble();
+		System.out.println((temperature - 32) / 1.8);
+	}
+	System.out.println ("Please enter 1 or 2");
+	
+    }
+}
